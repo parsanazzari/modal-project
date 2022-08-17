db16b45039f19481cc54b5d7beda9625b0d70ffa
parsa nazari <parsateo29@gmail.com>
2022-08-17 17:10:06 +0430

    using querySelector

---
+++ modal.js
@@ -0,0 +1,5 @@
+const showModalBtn = document.querySelector(".show-modal");
+const modal = document.querySelector(".modal");
+const backDrop = document.querySelector(".backdrop");
+const closeModal = document.querySelector(".close-modal");
+const confirmModal = document.querySelector(".confirm-modal");
