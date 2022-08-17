9784a0a0b49ac1b63bcff469020e1e76eeb4fa1a
parsa nazari <parsateo29@gmail.com>
2022-08-17 17:11:33 +0430

    using addEventListener

--- modal.js
+++ modal.js
@@ -3,3 +3,27 @@ const modal = document.querySelector(".modal");
 const backDrop = document.querySelector(".backdrop");
 const closeModal = document.querySelector(".close-modal");
 const confirmModal = document.querySelector(".confirm-modal");
+
+
+
+showModalBtn.addEventListener("click",()=>{
+  modal.style.opacity = "1";
+  modal.style.transform = "translateY(20vh)";
+  backDrop.style.display = "block";
+});
+
+function closeModalFunction(){
+modal.style.opacity = "0";
+modal.style.transform = "translateY(-100vh)";
+backDrop.style.display = "none";
+}
+
+closeModal.addEventListener("click" , closeModalFunction);
+backDrop.addEventListener("click" , closeModalFunction);
+
+confirmModal.addEventListener("click",()=>{
+  modal.style.opacity = "0";
+  modal.style.transform = "translateY(-100vh)";
+  backDrop.style.display = "none";
+  alert("confirm information")
+});
